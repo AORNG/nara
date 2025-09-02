f492dda94d6411ccf065c4494220c8ebe018ff1b
test point <167181348+AORNG@users.noreply.github.com>
2025-09-02 08:44:49 +0000

    a

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# p5.jsスケッチをHTMLで埋め込む
+p5_code = """
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js"></script>
+    <script>
+      function setup() {
+        createCanvas(400, 400);
+        background(220);
+      }
+
+      function draw() {
+        fill(100, 150, 255);
+        ellipse(mouseX, mouseY, 50, 50);
+      }
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
+"""
+
+st.title("Streamlit + p5.js デモ")
+components.html(p5_code, height=450)
